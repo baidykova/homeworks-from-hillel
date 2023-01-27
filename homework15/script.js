1c9c8af6212ed46bdcecc350041a0b1e787c6116
baidykova <ann.baidikova@icloud.com>
2023-01-27 23:16:12 +0200

    loops, arrays, array methods.

---
+++ homework15/script.js
@@ -0,0 +1,7 @@
+const fruits =  ["banana", "apple", "pear", "strawberry", "pineapple", "plum", "orange"];
+fruits.forEach(fruit => console.log(fruit));
+
+for(fruit of fruits) {
+	console.log(fruit);
+}
+
