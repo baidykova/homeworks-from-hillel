4431369950a5754c5aabc310fd629dcdd856c1e3
baidykova <ann.baidikova@icloud.com>
2023-03-10 22:41:57 +0200

    promise

---
+++ homework32/script.js
@@ -0,0 +1,46 @@
+const wakeUp = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("Waking up at 6:00 am.");
+    resolve("Good morning!");
+  }, 200);
+});
+
+
+const breakfast = wakeUp.then((result) => {
+  console.log("Preparing breakfast.");
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Breakfast is ready!");
+      resolve("Enjoy your meal!!");
+    }, 500);
+  });
+});
+
+
+const work = breakfast.then((result) => {
+  console.log("Preparation for work.");
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Time for work!");
+      resolve("Have a nice day at work!");
+    }, 200);
+  });
+});
+
+
+const dinner = work.then((result) => {
+  console.log("Cooking dinner.");
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Dinner is ready!");
+      resolve("Enjoy your meal!!");
+    }, 200);
+  });
+});
+
+
+dinner.then((result) => {
+  console.log("Time to relax!");
+}).catch((error) => {
+  console.log(error);
+});
